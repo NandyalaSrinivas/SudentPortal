1d5cb399309750c735de44416eeb96e7998403e4
NandyalaSrinivas <61131782+NandyalaSrinivas@users.noreply.github.com>
2020-12-02 00:14:27 +0530

    Update admin.py

--- StudentManagementProject/studentapp/admin.py
+++ StudentManagementProject/studentapp/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import RegisterModel, ApplicationModel
+from .models import Register, Application
 
 
-admin.site.register(RegisterModel)
-admin.site.register(ApplicationModel)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'marks_inter', 'is_selected']
+    ordering = ['marks_inter']
+    list_editable = ['is_selected']
+
+
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'department']
+    ordering = ['id']
+
+
+admin.site.register(Register, RegisterAdmin)
+admin.site.register(Application, ApplicationAdmin)
