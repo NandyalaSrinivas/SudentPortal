ce0df3fb0de28832b8b3aad7bc7ad260af12323d
NandyalaSrinivas <61131782+NandyalaSrinivas@users.noreply.github.com>
2020-12-02 08:11:44 +0530

    Update admin.py

--- StudentManagementProject/studentapp/admin.py
+++ StudentManagementProject/studentapp/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 from .models import Register, Application
+from django.contrib.admin import SimpleListFilter
 
+
+class Selected(admin.SimpleListFilter):
+    title = ('is_selected')
+    parameter_name = 'selected'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('True','selected',),
+            ('False','unselected'),
+        )
+
+
+    def queryset(self, request, queryset):
+        if self.value() == 'True':
+            return queryset.filter(marks_inter__gte=900)
+
+        if self.value() == 'False':
+            return queryset.filter(marks_inter__lte=900)
 
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'email', 'marks_inter', 'is_selected']
     ordering = ['marks_inter']
-    list_editable = ['is_selected']
+    list_editable = ['is_selected', 'Selected']
 
 
 class RegisterAdmin(admin.ModelAdmin):
